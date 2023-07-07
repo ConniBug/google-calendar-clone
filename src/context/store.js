2303f66e7013b7be952a9ad2cf079d74288f58fa
Conni Bilham <me@conni.lgbt>
2023-07-07 14:08:04 +0100

    Change url parsing for websocket url

--- src/context/store.js
+++ src/context/store.js
@@ -263,7 +263,7 @@ class Store {
     this.animationStatus = true;
 
     this.api_url = localStorage.getItem("api_url") || "https://api-cal.transgirl.space/api";
-    this.ws_url = localStorage.getItem("ws_url") || "api-cal.transgirl.space:8080";
+    this.ws_url = localStorage.getItem("ws_url") || "wss://api-cal.transgirl.space:8080";
     if(!localStorage.getItem("user")) {
       let str = JSON.stringify({ id: "123", token: "321", expiry: 0 });
       localStorage.setItem("user", str);
@@ -287,9 +287,21 @@ class Store {
         return;
       }
 
+      let ws = 0;
+      if(this.ws_url.startsWith("wss://")) {
+        let url = this.ws_url.replace("wss://", "");
+        ws = new WebSocket("wss://" + url, 'echo-protocol');
+      }
+      else if(this.ws_url.startsWith("ws://")) {
+        let url = this.ws_url.replace("wss://", "");
+        ws = new WebSocket("wss://" + url, 'echo-protocol');
+      }
+      else
+        ws = new WebSocket("wss://" + this.ws_url, 'echo-protocol');
+
       console.log("Connecting to websocket server. (" + this.ws_url + ")");
-      let ws = new WebSocket(this.ws_url, 'echo-protocol');
-      ws.root = this;
+      // let ws = new WebSocket(this.ws_url, 'echo-protocol');
+      // ws.root = this;
 
       ws.onopen = function () {
         l.debug("Websocket connection established.", "Websocket");
