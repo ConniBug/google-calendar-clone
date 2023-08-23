74d1daedfde0f4cafcc25c59ee7255fca75b4c17
Conni Bilham <me@conni.lgbt>
2023-08-23 16:55:43 +0100

    Remove buggy notif test

--- src/index.js
+++ src/index.js
@@ -63,8 +63,5 @@ renderViews(context, datepickerContext, store);
 if (window.Notification) {
     Notification.requestPermission((status) => {
         console.log('Status of the notification permission request:', status);
-
     });
-    let cont = "Test notif";
-    Notification.call(cont);
 }
