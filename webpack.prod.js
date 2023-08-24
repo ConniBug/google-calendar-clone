ecaaeb5fef2dc3bddb3546839d5a48842bf7d10b
Conni Bilham <me@conni.lgbt>
2023-08-24 14:24:19 +0100

    Modify webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,24 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const path = require("path");
+const fs = require("fs");
 
 module.exports = merge(common, {
     mode: 'production',
-    devtool: 'inline-source-map',
-
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        host: '0.0.0.0',
+        port: 30000,
+        allowedHosts: 'all',
+        open: false,
+        hot: true,
+        compress: true,
+    },
+    performance: {
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000
+    },
 });
